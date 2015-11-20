264eab3ad03407f6039627982f5f7e4a8470d8de
Mathew Ostrander <mathew.ostrander@gmail.com>
2015-11-20 10:13:38 -0600

    signup and login both push data to firebase now

--- javascripts/createuser.js
+++ javascripts/createuser.js
@@ -1,22 +1,23 @@
 define(["dependencies", "q"], function(_$_, Q) {
 
-	return {
-		createTheUser: function(emailIt, passwordIt, ref) {
+	function createTheUser(emailIt, passwordIt, ref) {
 
-			var deferred = Q.defer();
+		var deferred = Q.defer();
 
-			ref.createUser({
+		ref.createUser({
 			  email    : emailIt,
 			  password : passwordIt
 			}, function(error, userData) {
 			  if (error) {
 			    console.log("Error creating user:", error);
+			    deferred.reject(error);
 			  } else {
 			    console.log("Successfully created user account with uid:", userData.uid);
+			    deferred.resolve(userData);
 			  }
 			});
 	    return deferred.promise;
-		}
-	};
+	} //End creatheuser function
 
+	return createTheUser;
 });
