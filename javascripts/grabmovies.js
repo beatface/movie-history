d336fb6b465ebd863f537fbb910bce5c916a1b39
Andri Alexandrou <andri.j.alexandrou@gmail.com>
2015-11-23 09:42:06 -0600

    more firebase structure changes

--- javascripts/grabmovies.js
+++ javascripts/grabmovies.js
@@ -5,15 +5,13 @@ return {
     findMovie: function(moviedata, userID) {
       var thisUserLibrary = new Firebase("https://ama-moviehistory.firebaseio.com/all-users-libraries/user_library_" + userID);
       //grabs title value from modal
-      var userMovieInLibrary = thisUserLibrary.child(moviedata.Title + "_" + moviedata.Year);
-      userMovieInLibrary.set({
-        "omdb_data": moviedata,
-        "watched": false, // boolean
-        "rating": 0 // user will rate 1 to 5
-      });
+      // var userMovieInLibrary = thisUserLibrary.child(moviedata.Title + "_" + moviedata.Year);
+      var userMovieInLibrary = thisUserLibrary.child(moviedata.Title);
+      moviedata.rating = 0;
+      moviedata.watched = false;
+
+      userMovieInLibrary.set(moviedata);
+      }
         
-    }
-  };
-
-
+    };
 });
