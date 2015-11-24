f1ca70741e4621f3cff34295c6d1ea454475663f
Andri Alexandrou <andri.j.alexandrou@gmail.com>
2015-11-24 15:04:59 -0600

    added modal responsiveness to poster click

--- javascripts/add-modal.js
+++ javascripts/add-modal.js
@@ -5,6 +5,8 @@ define(["dependencies", "q"], function(_$_, Q) {
       	url: "https://ama-moviehistory.firebaseio.com/all-users-libraries/user_library_"+ passedAuth +"/" + movieKey + "/.json"
       	}).done(function(modalData) {
           $('#posterModal').modal();
+          console.log("modalData", modalData);
+          $(".modal-title").html(modalData.Title);
           require(['hbs!../templates/modal'], function(modalTemplate) {
             $("#modal-body").html(modalTemplate(modalData));
           });
